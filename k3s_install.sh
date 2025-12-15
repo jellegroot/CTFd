abbe93103f5f336aea2175d40d968ff4aa623013
Jelle Groot <jelle.groot@hotmail.nl>
2025-12-15 12:06:05 +0100

    added scripts

---
+++ k3s_install.sh
@@ -0,0 +1,11 @@
+# Setup Database mariadb
+
+sudo apt install mariadb-server
+sudo apt install mariadb-client-core
+
+
+CREATE DATABASE mijn_database
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
+
---
+++ k3s_setup.md
@@ -0,0 +1,3 @@
+# setup mysql database
+
+
